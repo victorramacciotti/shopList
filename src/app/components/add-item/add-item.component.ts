d0b2984a5bc0cc0dbd056879eebe71888f0da49f
Victor Araujo <victtordan10@gmail.com>
2024-10-08 17:28:18 -0300

    feat: botão para alterar isulização do form de add item

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { Item } from '../../../item';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../button/button.component';
  
 @Component({
   selector: 'app-add-item',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ButtonComponent],
   templateUrl: './add-item.component.html',
   styleUrl: './add-item.component.scss'
 })
@@ -33,6 +34,10 @@ export class AddItemComponent {
 
     this.nome = '';
     this.comprado = false;
+  }
+
+  changeVisibility(valor:boolean){
+    this.mostraAddItem = valor;
   }
 
 }
