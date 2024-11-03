f3261096baaa50f7850c98eb7493efc5c7f9bc96
Victor Araujo <victtordan10@gmail.com>
2024-11-02 21:33:31 -0300

    correções app.config.ts e main.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideAuth0({
+      domain: 'dev-7fvb7yro48pcduqm.us.auth0.com',
+      clientId: 'KZHWPMBGOnddUixc0aIwn2GNFqQZYQcc',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+    ] 
 };
